48727e26a9646b73e59bac368d79f66dcad6580b
sanvikadam <sanvi.kadam0104@gmail.com>
2020-03-08 23:23:44 +0530

    code cleanup

--- D3X-UI/src/app/app-routing.module.ts
+++ D3X-UI/src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { ProfileComponent } from './views/profile/profile.component';
-import { NewShipmentComponent } from './views/new-shipment/new-shipment.component';
-import { ShipmentDetailsComponent } from './views/shipment-details/shipment-details.component';
 import { ProfileDetailsComponent } from './layout/profile-details/profile-details.component';
 import { RecipientInformationComponent } from './layout/recipient-information/recipient-information.component';
 import { CustomerRecipientDetailsComponent } from './layout/customer-recipient-details/customer-recipient-details.component';
@@ -18,11 +15,11 @@ const appRoutes: Routes = [
       { path: '', redirectTo: 'profile', pathMatch: 'full' },
       {
         path: 'profile',
-        component : ProfileComponent
+        component : ProfileDetailsComponent
       },
       {
         path: 'create-shipment',
-        component : NewShipmentComponent
+        component : RecipientInformationComponent
       },
       {
         path: 'shipment-details',
@@ -30,7 +27,7 @@ const appRoutes: Routes = [
       },
       {
         path: 'cancel-shipment',
-        component: NewShipmentComponent
+        component: ProfileDetailsComponent
       }
     ]
   }
--- D3X-UI/src/app/app.module.ts
+++ D3X-UI/src/app/app.module.ts
@@ -5,21 +5,17 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RecipientInformationComponent } from './layout/recipient-information/recipient-information.component';
 import { OrderSummaryInformationComponent } from './layout/order-summary-information/order-summary-information.component';
 import { OrderLineItemsComponent } from './layout/order-line-items/order-line-items.component';
-import { NewShipmentComponent } from './views/new-shipment/new-shipment.component';
-import { ShipmentDetailsComponent } from './views/shipment-details/shipment-details.component';
 import { OrderSummaryComponent } from './layout/order-summary/order-summary.component';
 import { CustomerRecipientDetailsComponent } from './layout/customer-recipient-details/customer-recipient-details.component';
 import { DeliveryShipmentDetailsComponent } from './layout/delivery-shipment-details/delivery-shipment-details.component';
 import { LoginComponent } from './views/login/login.component';
-import { ProfileComponent } from './views/profile/profile.component';
 import { LoginFormComponent } from './layout/login-form/login-form.component';
 import { SidebarNavigationComponent } from './layout/sidebar-navigation/sidebar-navigation.component';
 import { ProfileDetailsComponent } from './layout/profile-details/profile-details.component';
-import { ViewPageComponent } from './views/view-page/view-page.component';
 import { OrderQuoteComponent } from './layout/order-quote/order-quote.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 
@@ -29,17 +25,13 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
     RecipientInformationComponent,
     OrderSummaryInformationComponent,
     OrderLineItemsComponent,
-    NewShipmentComponent,
-    ShipmentDetailsComponent,
     OrderSummaryComponent,
     CustomerRecipientDetailsComponent,
     DeliveryShipmentDetailsComponent,
     LoginComponent,
-    ProfileComponent,
     LoginFormComponent,
     SidebarNavigationComponent,
     ProfileDetailsComponent,
-    ViewPageComponent,
     OrderQuoteComponent,
     DashboardComponent
   ],
@@ -47,7 +39,7 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    // BrowserAnimationsModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule
   ],

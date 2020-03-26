72b5652c46d5244e4ddb2aa17dc611a238d119c8
sanvikadam <sanvi.kadam0104@gmail.com>
2020-03-26 18:06:24 +0530

    Worked on delivery shipment details

--- D3X-UI/src/app/layout/delivery-shipment-details/delivery-shipment-details.component.ts
+++ D3X-UI/src/app/layout/delivery-shipment-details/delivery-shipment-details.component.ts
@@ -11,11 +11,50 @@ export class DeliveryShipmentDetailsComponent implements AfterContentChecked {
 
   quotes: any = [];
 
+  cols: any = [];
+
+  first: number = 0;
+
+  last: number = 0;
+
+  rows: number = 10;
+
   constructor(public dataService: DataserviceService) { 
     
   }
 
   ngAfterContentChecked() {
     this.quotes = this.dataService.quote;
+
+    this.cols = [
+      {fields: 'name', header:'Name'},
+      {fields: 'shipdate', header:'Ship Date'},
+      {fields: 'estimatedate', header:'Estimated Date'},
+      {fields: 'estimateday', header:'Estiamted Day'},
+      {fields: 'servicetype', header:'Service Type'},
+      {fields: 'trackable', header:'Trackable'},
+      {fields: 'price', header:'Price'},
+      {fields: 'order', header:'Order'},
+    ]
+  }
+
+  next(){
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.first === (this.quotes.length - this.rows);
+  }
+
+  isFirstPage(): boolean {
+    return this.first === 0;
   }
 }

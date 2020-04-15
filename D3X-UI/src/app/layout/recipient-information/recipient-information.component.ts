db3600053a899539772f406480c18ff869c7dd58
sanvikadam <sanvi.kadam0104@gmail.com>
2020-04-15 17:59:54 +0530

    Worked on UI changes

--- D3X-UI/src/app/layout/recipient-information/recipient-information.component.ts
+++ D3X-UI/src/app/layout/recipient-information/recipient-information.component.ts
@@ -11,8 +11,11 @@ import { Router } from '@angular/router';
 export class RecipientInformationComponent implements OnInit{
   public showQuotes: boolean;
   public loading: boolean;
+  public error: boolean;
   public passData: string;
   public finalData: any;
+  public addressFlag: boolean; 
+  public adderror: boolean;
 
   constructor(
       private _shipmentquote: DataserviceService,
@@ -34,13 +37,7 @@ export class RecipientInformationComponent implements OnInit{
   price: null;
 
   ngOnInit(){
-    // this.packWidth = 1;
-    // this.packHeight = 1;
-    // this.packLength = 1;
-    // this.packWeight = 1;
 
-    
-    
     this.passData =  JSON.stringify({
       "delivery_list": [{
         "delivery_id": "0080017667",
@@ -51,12 +48,12 @@ export class RecipientInformationComponent implements OnInit{
         "address_from_state": "CA",
         "address_from_postalcode": "90066",
         "address_from_country": "US",
-        "address_to_line1": "4009 Lincoln Blvd",
+        "address_to_line1": "",
         "address_to_line2": "",
-        "address_to_city": "Marina Del Rey",
-        "address_to_state": "CA",
-        "address_to_postalcode": "90292",
-        "address_to_country": "US",
+        "address_to_city": "",
+        "address_to_state": "",
+        "address_to_postalcode": "",
+        "address_to_country": "",
         "validate_address": "no_validation",
         "package_weight": "1",
         "package_weight_unit": "ounce",
@@ -74,35 +71,75 @@ export class RecipientInformationComponent implements OnInit{
 
     this.finalData = convertData.delivery_list[0];
 
-    this.pickupAddr = this.finalData.address_from_line1+", "+this.finalData.address_from_city+ ", " +this.finalData.address_from_state+ ", " +this.finalData.address_from_country+ ", " +this.finalData.address_from_postalcode;
+    this.pickupAddr = this.finalData.address_from_line1+", "+this.finalData.address_from_city+ ", " +this.finalData.address_from_state+ ", " +this.finalData.address_from_postalcode+ ", " +this.finalData.address_from_country;
   }
   
   public saveShipment(): void {
-    this.loading=true;
+    // this.loading=true;
     this.showQuotes=false;
+    this.error=false;
 
+    let fromAddress = this.pickupAddr.split(',');
     let toAddress = this.dropofAddr.split(',');
-    let pinCode = toAddress[2].split(' ');
-    // console.log(toAddress, "and" ,pinCode);
     
-    // Code that actually needs to be run
+    // Code that split and send data
+    console.log(toAddress.length);
+    console.log(toAddress);
 
-    this.finalData.address_to_line1 = toAddress[0];
-    this.finalData.address_to_city = toAddress[1];
-    this.finalData.address_to_state = pinCode[1];
-    this.finalData.address_to_postalcode = pinCode[2];
+    if(fromAddress.length < 5 || toAddress.length < 5) {
 
-    // console.log("Hello " +JSON.stringify({"delivery_list": [this.finalData]}));
+      this.addressFlag = false;
+        console.log("You have an error")
+
+    } else {
+      this.adderror = false;
+      this.loading = true;
+      this.addressFlag = true;
+      this.finalData.address_to_line1 = toAddress[0].trim();
+      this.finalData.address_to_city = toAddress[1].trim();
+      this.finalData.address_to_state = toAddress[2].trim();
+      this.finalData.address_to_postalcode = toAddress[3].trim();
+      this.finalData.address_to_country = toAddress[4].trim();
+
+      this.finalData.address_from_line1 = fromAddress[0].trim();
+      this.finalData.address_from_city = fromAddress[1].trim();
+      this.finalData.address_from_state = fromAddress[2].trim();
+      this.finalData.address_from_postalcode = fromAddress[3].trim();
+      this.finalData.address_from_country = fromAddress[4].trim();
+    }
+
+    console.log(this.finalData);
 
     let data = JSON.stringify({"delivery_list": [this.finalData]});
 
     let reqUrl = 'https://s0020806703trial-trial.apim1.hanatrial.ondemand.com:443/s0020806703trial/http/get_delv_quote_multi_bck/json';//'assets/local/ship-details.json';
 
  this.httpClient.post(reqUrl, data).subscribe(res=> {
-      this._shipmentquote.getQuote(res);
-      this.showQuotes = true;
-      this.loading = false;
+
+      if(this.addressFlag == false) {
+        console.log('hello there');
+        this.loading = false;
+        this.error = false;
+        this.showQuotes = false;
+        this.adderror = true;
+      } else{
+        this.error = false;
+        this.adderror = false;
+        this._shipmentquote.getQuote(res);
+        this.showQuotes = true;
+        this.loading = false;
+      }
     })
     //this.router.navigate(["/dashboard/shipment-details"]);
   }
+
+  clear() {
+    this.error = false;
+    this.adderror = false;
+    this.showQuotes = false;
+    this.loading = false;
+    this.pickupAddr = null;
+    this.dropofAddr = null;
+  }
+
 }
--- D3X-UI/src/js/script.js
+++ D3X-UI/src/js/script.js
@@ -8,17 +8,20 @@ $(document).ready(function(){
         cols += '<td><input type="text" class="form-control" name="sku' + counter + '"/></td>'
         cols += '<td><input type="text" class="form-control" name="name' + counter + '"/></td>';
         cols += '<td><input type="text" class="form-control" name="quantity' + counter + '"/></td>';
-        cols += '<td class="input-group"><span class="input-group-addon">$</span><input type="text" class="form-control" value="0" name="price' +counter+ '"/></td>';
-        cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';
+        cols += '<td><input type="text" class="form-control" name="price' +counter+ '"/></td>';
+        cols += '<td><a href="#" class="btn btn-danger btn-icon-split ibtnDel">'+
+                '<span class="icon text-white-50"><i class="fas fa-trash"></i></span>'+
+                '</a></td>';
         newRow.append(cols);
-        $("table.order-list").append(newRow);
+        $("table.line-items").append(newRow);
         if(counter == 9) {
             $("#addrow").addClass('disabled').attr('disabled', true);
         }
         counter++;
     });
 
-    $("body").on("click", "table.order-list .ibtnDel", function (event) {
+    $("body").on("click", "table.line-items .ibtnDel", function (event) {
+        event.preventDefault();
         $(this).closest("tr").remove();
         counter -= 1
          if(counter <= 9) {

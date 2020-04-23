e3b8679b6faf5f4cd19c508c38a6ae26c335e4d0
sanvikadam <sanvi.kadam0104@gmail.com>
2020-04-23 18:47:05 +0530

    code enhancement

--- D3X-UI/src/app/app.module.ts
+++ D3X-UI/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { PathLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -44,7 +45,10 @@ import { FooterComponent } from './layout/footer/footer.component';
     ReactiveFormsModule,
     TableModule
   ],
-  providers: [],
+  providers: [{
+    provide: LocationStrategy, 
+    useClass: PathLocationStrategy
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

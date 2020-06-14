f93c788488cd8b54c8298bd099fb6a6567386ec0
Abhi <abhi5885@gmail.com>
2020-06-14 11:54:42 +0530

    updated changes

--- D3X-UI/src/app/layout/recipient-information/recipient-information.component.ts
+++ D3X-UI/src/app/layout/recipient-information/recipient-information.component.ts
@@ -230,6 +230,7 @@ export class RecipientInformationComponent implements OnInit{
     // console.log(this.finalData);
 
     let data = JSON.stringify({"delivery_list": [this.finalData]});
+    console.log(data)
 
     let reqUrl = 'https://s0020806703trial-trial.apim1.hanatrial.ondemand.com:443/s0020806703trial/http/get_delv_quote_multi_bck/json';//'assets/local/ship-details.json';
 
